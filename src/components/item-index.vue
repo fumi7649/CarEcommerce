3397e7aaeeb156c5c7e70e7980740bccf193086d
fumi <fumifumi7629@gmail.com>
2022-03-13 22:24:24 +0900

    preparation img

---
+++ src/components/item-index.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+import {Item} from "@/module/item";
+export default {
+  name : 'item-index',
+  data : {
+    return(){
+      items : [
+        new Item("Model3", "Tesla", "$48,490", "2020-10-4", ),
+        
+      ]
+    }
+  }
+}
+</script>
