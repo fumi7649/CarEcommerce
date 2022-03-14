d853aa68f2f591bf676474bea2fd35648faa05f6
fumi <fumifumi7629@gmail.com>
2022-03-14 12:53:51 +0900

    createItemCardComponent

--- src/components/item-card.vue
+++ src/components/item-card.vue
@@ -1,12 +1,27 @@
 <template>
-  
+  <div class="card m-2" style="width: 18rem;">
+    <div class="card-body">
+      <img :src="imgSrc" class="card-img-top" />
+      <h5 class="card-title pt-2">{{item.name}}</h5>
+      <p class="card-text">Category: {{item.category}}</p>
+      <p class="card-text">Price: {{item.price}}</p>
+      <p class="card-text">Date: {{item.data}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-   name : 'item-card',
-   props : {
-     item : Object
-   }
-}
+  name: "item-card",
+  props: {
+    item: Object,
+  },
+  computed: {
+    imgSrc(){
+      
+      const img = "/img/" + this.item.img + ".jpg";
+      return img;
+    }
+  }
+};
 </script>
